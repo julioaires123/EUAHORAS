092456db9c7a2b3dba6014ea6a14b1c5f2a88ebe
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2022-11-30 21:19:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let semanas = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
 let data = new Date();
 let diasem = data.getDay()+1;
 let dia = data.getDate();
-let mes = data.getMonth();
+let mes = data.getMonth()+1;
 let ano = data.getFullYear();
 document.write(semanas[diasem] + ", " + dia + " " + meses[mes] + " " + ano);   
     
