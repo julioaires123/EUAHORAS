41724efd17fc618505ac77e3cfc79a8da36daa2c
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2022-11-30 21:31:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,14 +23,13 @@
 
         
     
-
 //Dia mês e ano /////////////////////////////////////////////////////////////////////////////////////////////
 let meses = new Array( "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 let semanas = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
 let data = new Date();
-let diasem = data.getDay();
-let dia = data.getDate();
-let mes = data.getMonth();
-let ano = data.getFullYear();
-document.write(semanas[diasem] + ", " + dia + " " + meses[mes] + " " + ano);   
+let diasem = data.getUTCDay();
+let dia = data.getUTCDate();
+let mes = data.getUTCMonth();
+let ano = data.getUTCFullYear();
+document.write(semanas[diasem] + ", " + dia + " " + meses[mes] + " " + ano);     
     
