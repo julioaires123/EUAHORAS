886bd157310d7d9196b6906824e66d92007fbcee
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2023-12-26 18:58:34 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const newYear = "1/1/2024";
 
-data.setSecondEl(data.getSecondEl () +20);
+document.setSecondEl(document.getSecondEl () +20);
 
 const daysEl = document.querySelector('.Dias');
 const hourEl = document.querySelector('.Horas');
