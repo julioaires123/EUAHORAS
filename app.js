e6c6d87ae2d8bbd6ebd1250e8eeee3fe163d1222
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2024-10-23 20:00:13 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,15 +8,15 @@ const secondEl = document.querySelector('.Segundos');
 function timeCountDown() {
   const nowDate = new Date();
   const newYearDate = new Date(newYear);
-  let totalSeconds = (newYearDate - nowDate) / 1000;
+  let totalSeconds = Math.floor((newYearDate - nowDate) / 1000);
 
   // Subtraindo 20 segundos e adicionando 2 segundos
-  totalSeconds = totalSeconds - 20 + 3; // Isso resulta em uma subtração total de 18 segundos
+  totalSeconds = totalSeconds - 20 + 2; // Total de 18 segundos
 
   const Dias = Math.floor(totalSeconds / 3600 / 24);
-  const Horas = Math.floor(totalSeconds / 3600) % 24;
-  const Minutos = Math.floor(totalSeconds / 60) % 60;
-  const Segundos = Math.floor(totalSeconds) % 60;
+  const Horas = Math.floor((totalSeconds % 86400) / 3600);
+  const Minutos = Math.floor((totalSeconds % 3600) / 60);
+  const Segundos = totalSeconds % 60;
 
   daysEl.textContent = formatTime(Dias);
   hourEl.textContent = formatTime(Horas);
