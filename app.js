323d3406157a00b3c1052011a6ea7613b33cc6cc
JÚLIO CARLOS DE OLIVEIRA AIRES <52129925+julioaires123@users.noreply.github.com>
2025-11-27 13:48:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ function timeCountDown() {
   let totalSeconds = Math.floor((newYearDate - nowDate) / 1000);
   
   // Incrementando 9 segundos
-  totalSeconds -= 00;
+  totalSeconds -= 16;
 
   // Cálculos
   const Dias = Math.floor(totalSeconds / 3600 / 24);
